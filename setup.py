a8af9422cc094c477f6a01d949d226e5978791ff
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2021-07-26 17:56:24 -0400

    Packageify

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="small_sample_mean_bounds",
+    version="0.1",
+    description="Small sample mean bounds",
+    url="http://github.com/myphan9/small_sample_mean_bounds",
+    author="My Phan",
+    author_email="",
+    license="",
+    packages=["small_sample_mean_bounds"],
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+    zip_safe=False,
+)
+
